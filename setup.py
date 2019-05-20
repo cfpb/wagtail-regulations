fdccfd9180dc9e4e61fa631e01e55d3d240cd898
Will Barton <william.barton@cfpb.gov>
2019-05-20 16:27:05 -0400

    Initial Regtail Skeleton

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages, setup
+
+
+long_description = open('README.md', 'r').read()
+
+install_requires = [
+    'wagtail>=1.13,<2.4',
+    'django-flags>=4.0,<5.0'
+]
+
+testing_extras = [
+    'coverage>=3.7.0',
+]
+
+setup(
+    name='wagtail-regulations',
+    url='https://github.com/cfpb/wagtail-flags',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    description='Building blocks for interactive regulations in Wagtail',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='CC0',
+    version='4.0.2',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 1',
+        'Framework :: Wagtail :: 2',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'License :: Public Domain',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
