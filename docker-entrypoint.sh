dfa88817af607c575f461b3c52344571c5b0b063
Will Barton <william.barton@cfpb.gov>
2020-01-28 23:33:11 -0500

    Update Docker setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,4 @@ done
 
 >&2 echo "Postgres is up - continuing"
 
-if [ "$1" = '/venv/bin/uwsgi' ]; then
-    /venv/bin/python manage.py migrate --noinput
-fi
-
-if [ "x$DJANGO_LOAD_INITIAL_DATA" = 'xon' ]; then
-	/venv/bin/python manage.py load_initial_data
-fi
-
 exec "$@"
